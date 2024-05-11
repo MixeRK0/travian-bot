d81040782692f3e80183a9e3d2d6cb6a293fbb4d
Nikolay <arhangelskinikolay@gmail.com>
2024-05-11 18:06:56 +0300

    temp

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,17 @@ import (
 	"time"
 	"travian-bot/common"
 	send_troops "travian-bot/send-troops"
+	train_troops "travian-bot/train-troops"
 )
 
 func main() {
 	common.Login()
 
 	go send_troops.SendTroops(common.ImpTargets, "Imperatoris")
-	//time.Sleep(time.Second * 333)
+	time.Sleep(time.Second * 333)
 	//go send_troops.SendTroops(common.LegsTargets, "Legs")
 	//time.Sleep(time.Second * 111)
-	//go train_troops.TrainTroops()
+	go train_troops.TrainTroops()
 
 	for {
 		time.Sleep(time.Minute * 100)
