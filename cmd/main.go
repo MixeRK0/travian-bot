0f5c9fc854396be3e9ddab03192fd0cbc443a263
Nikolay <arhangelskinikolay@gmail.com>
2024-05-12 04:15:31 +0300

    temp

--- cmd/main.go
+++ cmd/main.go
@@ -12,8 +12,8 @@ func main() {
 	go send_troops.SendTroops(common.ImpTargets, "Imperatoris")
 	//time.Sleep(time.Second * 333)
 	//go send_troops.SendTroops(common.LegsTargets, "Legs")
-	//time.Sleep(time.Second * 111)
-	//go train_troops.TrainTroops()
+	time.Sleep(time.Second * 60 * 30)
+	go train_troops.TrainTroops()
 
 	for {
 		time.Sleep(time.Minute * 100)
